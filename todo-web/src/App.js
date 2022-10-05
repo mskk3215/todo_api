af79ac649b1af86ed150f728e3c47a61abf7240f
mskk3215 <m.kaaiweb@gmail.com>
2022-10-05 22:04:05 +0900

    task element modify

--- todo-web/src/App.js
+++ todo-web/src/App.js
@@ -1,13 +1,56 @@
-import './App.css';
-import { Task } from './components/Task';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+import { Task } from "./components/Task";
+import { Box, Center, Text, CheckboxGroup } from "@chakra-ui/react";
 
 export default function App() {
+  const initialTasks = [
+    { name: "買い物", isDone: true },
+    { name: "ランニング", isDone: false },
+    { name: "プログラミングの勉強", isDone: false },
+  ];
+
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(initialTasks);
+  }, []);
+
+  //toggle:on/off切り替えの意味
+  const toggleIsDone = (index) => {
+    // 一旦tasksを変数に入れることでsetTaskが機能するようにする
+    const tasksCopy = [...tasks];
+    const isDone = tasksCopy[index].isDone;
+    tasksCopy[index].isDone = !isDone;
+    setTasks(tasksCopy);
+  };
+
   return (
-    <>
-      <h1>タスク一覧</h1>
-      <Task name="買い物" />
-      <Task name="ランニング" />
-      <Task name="プログラミングの勉強" />
-    </>
+    <Box mt="64px">
+      <Center>
+        <Box>
+          <Box mb="24px">
+            <Text fontSize="24px" fontWeight="bold">
+              タスク一覧
+            </Text>
+          </Box>
+          <CheckboxGroup>
+            {tasks.map((task, index) => {
+              return (
+                <Task
+                  key={index}
+                  index={index}
+                  name={task.name}
+                  isDone={task.isDone}
+                  // 関数を渡す
+                  toggleIsDone={toggleIsDone}
+                ></Task>
+              );
+            })}
+          </CheckboxGroup>
+        </Box>
+      </Center>
+    </Box>
   );
 }
--- todo-web/src/components/Task.jsx
+++ todo-web/src/components/Task.jsx
@@ -1,8 +1,18 @@
-export const Task =(props)=>{
-  return(
-    <>
-    <li>{props.name}</li>
-    </>
-  )
+import { Checkbox, Box, Text } from "@chakra-ui/react";
 
-}
+export const Task = (props) => {
+  return (
+    <Box mb="16px">
+      <Checkbox
+        isChecked={props.isDone}
+        colorScheme="blue"
+        size="lg"
+        onChange={() => {
+          props.toggleIsDone(props.index);
+        }}
+      >
+        <Text>{props.name}</Text>
+      </Checkbox>
+    </Box>
+  );
+};
