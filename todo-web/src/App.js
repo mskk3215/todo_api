5aa77d75fde4e207004f7c75f442e0c65180044d
mskk3215 <m.kaaiweb@gmail.com>
2022-10-06 00:29:38 +0900

    rails api setting add

--- todo-web/src/App.js
+++ todo-web/src/App.js
@@ -4,24 +4,27 @@ import React, { useState, useEffect } from "react";
 import { Task } from "./components/Task";
 import { Box, Center, Text, CheckboxGroup } from "@chakra-ui/react";
 
-export default function App() {
-  const initialTasks = [
-    { name: "買い物", isDone: true },
-    { name: "ランニング", isDone: false },
-    { name: "プログラミングの勉強", isDone: false },
-  ];
+import axios from "axios";
 
+export default function App() {
   const [tasks, setTasks] = useState([]);
+  //非同期関数asyncでawaitキーワードを使用することでpromise(resolve, reject)の結果が返されるまで待機する
+  const fetch = async () => {
+    //axios.getでrailsとapi通信
+    const res = await axios.get("http://localhost:3010/tasks");
+    // console.log(res);
+    setTasks(res.data);
+  };
 
   useEffect(() => {
-    setTasks(initialTasks);
+    fetch();
   }, []);
 
   //toggle:on/off切り替えの意味
   const toggleIsDone = (index) => {
     // 一旦tasksを変数に入れることでsetTaskが機能するようにする
     const tasksCopy = [...tasks];
-    const isDone = tasksCopy[index].isDone;
+    const isDone = tasks[index].is_done;
     tasksCopy[index].isDone = !isDone;
     setTasks(tasksCopy);
   };
@@ -42,10 +45,10 @@ export default function App() {
                   key={index}
                   index={index}
                   name={task.name}
-                  isDone={task.isDone}
+                  isDone={task.is_done}
                   // 関数を渡す
                   toggleIsDone={toggleIsDone}
-                ></Task>
+                />
               );
             })}
           </CheckboxGroup>
