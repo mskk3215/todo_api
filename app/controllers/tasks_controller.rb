c6f6093916b02d42964b98ea0b55a86aa45d5e8b
mskk3215 <m.kaaiweb@gmail.com>
2022-10-07 00:06:20 +0900

    rails crud add

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,9 +2,32 @@ class TasksController < ApplicationController
 
   def index
     tasks = Task.all
+    # 完全なレスポンスを作成してブラウザに送信
     render json: tasks 
   end
 
+  def createw
+    Task.create(task_params)
+    head :created
+  end
 
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    # httpヘッダのみのレスポンスを作成してブラウザに送信
+    head :ok
+  
+  end
 
+  def update
+    task= Task.find(params[:id])
+    task.update(task_params)
+    head :ok
+  end
+
+  private
+  
+  def task_params
+    params.permit(:name, :is_done)
+  end
 end
--- todo-web/src/App.js
+++ todo-web/src/App.js
@@ -2,18 +2,43 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 
 import { Task } from "./components/Task";
-import { Box, Center, Text, CheckboxGroup } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Text,
+  CheckboxGroup,
+  Flex,
+  Input,
+  Button,
+} from "@chakra-ui/react";
 
 import axios from "axios";
 
 export default function App() {
   const [tasks, setTasks] = useState([]);
+  const [name, setName] = useState("");
   //非同期関数asyncでawaitキーワードを使用することでpromise(resolve, reject)の結果が返されるまで待機する
+
   const fetch = async () => {
     //axios.getでrailsとapi通信
     const res = await axios.get("http://localhost:3010/tasks");
-    // console.log(res);
     setTasks(res.data);
+  };
+
+  const createTask = async () => {
+    await axios.post("http://localhost:3010/tasks", {
+      name: name,
+      is_done: false,
+    });
+    //input値をリセット
+    setName("");
+    //タスクの再取得
+    fetch();
+  };
+
+  const destroyTask = async (id) => {
+    await axios.delete(`http://localhost:3010/tasks/${id}`);
+    fetch();
   };
 
   useEffect(() => {
@@ -21,12 +46,16 @@ export default function App() {
   }, []);
 
   //toggle:on/off切り替えの意味
-  const toggleIsDone = (index) => {
+  const toggleIsDone = async (id, index) => {
     // 一旦tasksを変数に入れることでsetTaskが機能するようにする
-    const tasksCopy = [...tasks];
+    // const tasksCopy = [...tasks];
     const isDone = tasks[index].is_done;
-    tasksCopy[index].isDone = !isDone;
-    setTasks(tasksCopy);
+    // tasksCopy[index].isDone = !isDone;
+    // setTasks(tasksCopy);
+    await axios.put(`http://localhost:3010/tasks/${id}`, {
+      is_done: !isDone,
+    });
+    fetch();
   };
 
   return (
@@ -38,16 +67,31 @@ export default function App() {
               タスク一覧
             </Text>
           </Box>
+
+          <Flex mb="24px">
+            <Input
+              placeholder="タスク名を入力"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <Box ml="16px">
+              <Button colorScheme="teal" onClick={createTask}>
+                タスクを作成
+              </Button>
+            </Box>
+          </Flex>
+
           <CheckboxGroup>
             {tasks.map((task, index) => {
               return (
                 <Task
-                  key={index}
+                  id={task.id}
                   index={index}
                   name={task.name}
                   isDone={task.is_done}
                   // 関数を渡す
                   toggleIsDone={toggleIsDone}
+                  destroyTask={destroyTask}
                 />
               );
             })}
